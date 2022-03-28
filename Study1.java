37c2d1a0cf79095f25700a1773fcc07c4da85567
Fu2kCodingArtist <suffer1030>
2022-03-28 23:25:42 +0900

    3.28

---
+++ Study1.java
@@ -0,0 +1,9 @@
+public class Study1 {
+    public static void main(String[]args) {
+        int result = 4 * 1;
+        System.out.printin(result);
+
+        result = 4 * 2;
+        System.out.printin(result)
+    }
+}
